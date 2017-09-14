f955ef345e54e034a604785e6d3a40f345c031a3
John Lange <lange.john.m@gmail.com>
2017-09-14 18:20:44 -0400

    Add undo cause Brad and Edd were sad

--- WebDraw/App_Start/BundleConfig.cs
+++ WebDraw/App_Start/BundleConfig.cs
@@ -15,7 +15,7 @@ namespace WebDraw
                         "~/Scripts/jquery.validate*"));
             
             bundles.Add(new ScriptBundle("~/bundles/draw").Include(
-            "~/Scripts/atrament.min.js"));
+            "~/Scripts/drawingboard.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -28,7 +28,8 @@ namespace WebDraw
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/drawingboard.min.css"));
         }
     }
 }
